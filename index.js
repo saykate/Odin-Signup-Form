f31bf9e123d07c2fd8ad04f06b43551b5034e988
Katie Sayers <kate@student.dev-island.org>
2023-08-16 11:35:13 -1000

    didn't submit properly, trying again

---
+++ index.js
@@ -0,0 +1,15 @@
+// let password = document.querySelector('.password')
+// let confirm = document.querySelector('.confirm_password')
+
+// const checkPass = () => {
+//     if(password = '') {
+//         alert ("Please enter password")
+//     } else if (confirm = '') {
+//         alert ("Please confirm password")
+//     } else if(confirm.value !== password.value) {
+//         alert ("Password did not match")
+//         return false
+//     } else {
+//         return true
+//     }
+// }
